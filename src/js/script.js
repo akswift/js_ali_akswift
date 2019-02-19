3cbc70915a9e16cedde3437a44747e34a185aa5d
akswift <akswift.freelance@gmail.com>
2019-02-19 20:33:21 +0200

    Домашнее задание с первого и второго урока

---
+++ src/js/script.js
@@ -0,0 +1,111 @@
+const cartWrapper = document.querySelector('.cart__wrapper'),
+  cart = document.querySelector('.cart'),
+  close = document.querySelector('.cart__close'),
+  open = document.querySelector('#cart'),
+  goodsBtn = document.querySelectorAll('.goods__btn'),
+  products = document.querySelectorAll('.goods__item'),
+  confirm = document.querySelector('.confirm'),
+  badge = document.querySelector('.nav__badge'),
+  totalCost = document.querySelector('.cart__total > span'),
+  titles = document.querySelectorAll('.goods__title');
+
+//Открываем и закрываем модальное окно корзины
+
+function openCart() {
+  cart.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+}
+
+function closeCart() {
+    cart.style.display = 'none';
+    document.body.style.overflow = '';
+}
+
+open.addEventListener('click', openCart);
+close.addEventListener('click', closeCart);
+
+//Добaвляем карточку товара в корзину товара 
+
+goodsBtn.forEach(function(btn, i) {
+  btn.addEventListener('click', () => {
+    let item = products[i].cloneNode(true),
+      trigger = item.querySelector('button'),
+      removeBtn = document.createElement('div'),
+      empty = cartWrapper.querySelector('.empty');
+
+    trigger.remove();
+
+    showConfirm();
+    calcGoods(1);
+
+    removeBtn.classList.add('goods__item-remove');
+    removeBtn.innerHTML = '&times';
+    item.appendChild(removeBtn);
+
+    cartWrapper.appendChild(item);
+    if (empty) {
+      empty.style.display = 'none';
+    }
+    calcTotal();
+    removeFromCart();
+  });
+//Подрезаем лишние буквы в тайтле
+  titles.forEach(function(item) {
+    if (item.textContent.length < 70) {
+      return;
+    } else {
+      const str = item.textContent.slice(0, 71) + '...';
+      item.textContent = str;
+    }
+  });
+// Анимируем с боку плавающую корзину
+  function showConfirm() {
+    confirm.style.display = 'block';
+    let counter = 100;
+    const id = setInterval(frame, 10);
+
+    function frame() {
+      if ( counter == 10) {
+        clearInterval(id);
+        confirm.style.display = 'none';
+      } else {
+      counter--;
+      confirm.style.transform = `translateY(-${counter}px)`;
+      confirm.style.opacity = '.' + counter;
+      }
+    }
+  }
+//Счетчик товаров в корзине
+  function calcGoods(i) {
+    const items = cartWrapper.querySelectorAll('.goods__item');
+    badge.textContent = i + items.length;
+  }
+//Счетчик общей суммы за товар
+  function calcTotal() {
+      const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price > span');
+      let total = 0;
+      prices.forEach(function(item) {
+        total += +item.textContent;
+      });
+      totalCost.textContent = total;
+  }
+//Удалние из корзины
+  function removeFromCart() {
+    const removeBth = cartWrapper.querySelectorAll('.goods__item-remove'),
+          items = cartWrapper.querySelectorAll('.goods__item'),
+          empty = cartWrapper.querySelector('.empty');
+
+    removeBth.forEach(function(btn) {
+      btn.addEventListener('click', () => {
+        btn.parentElement.remove();
+        calcGoods(0);
+        calcTotal();
+
+        if (items.length-1 == 0) {
+          empty.style.display = 'block';
+        }
+      });
+    });
+  }
+});
+
